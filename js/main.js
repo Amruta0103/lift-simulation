35967b26361c17d61133bb6c8bc40d1f66f8e8be
Amruta_D <71976347+Amruta0103@users.noreply.github.com>
2023-08-13 22:47:42 +0530

    trial

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
 const floorsVal = document.getElementById('floorsVal');
-const liftsVal = document.getElementById('liftsVal');
+// const liftsVal = document.getElementById('liftsVal');
 const submitBtn = document.getElementById('submitBtn');
 const display = document.getElementById('display');
 const allLvl = document.getElementById('allLevelsDiv');
 
-let flVal, lftVal ;
+let flVal;
+// let lftVal ;
 let flArr = [];
-let lftArr = [];
+// let lftArr = [];
 
 floorsVal.addEventListener('input',(e) => {
   flVal = (e.target.value);
 })
 
-liftsVal.addEventListener('input',(e) => {
-  lftVal = (e.target.value);
-})
+// liftsVal.addEventListener('input',(e) => {
+//   lftVal = (e.target.value);
+// })
 
 submitBtn.addEventListener('click',() => {
   for (var i=1; i<= flVal ; i++){
     flArr.push(i);
   }
-  for (var i=1; i<= lftVal ; i++){
-    lftArr.push(i);
-  }
-  display.innerText = (flArr,"\n",lftArr)
+  // for (var i=1; i<= lftVal ; i++){
+  //   lftArr.push(i);
+  // }
+  display.innerText = (flArr)
   flArr.forEach((i)=>{
     let item = document.createElement('div');
     item.textContent = i;
@@ -31,5 +32,6 @@ submitBtn.addEventListener('click',() => {
     allLvl.appendChild(item);
   });
   flArr.splice(0, flVal);
-  lftArr.splice(0,lftVal);
+  console.log(flArr);
+  // lftArr.splice(0,lftVal);
 })
