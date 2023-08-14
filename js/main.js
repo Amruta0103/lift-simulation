c3757382153a30a65e42711b59da2323d9c8c3b6
Amruta_D <71976347+Amruta0103@users.noreply.github.com>
2023-08-14 12:56:50 +0530

    added floor UI
    To add lifts

--- js/main.js
+++ js/main.js
@@ -1,37 +1,75 @@
 const floorsVal = document.getElementById('floorsVal');
 // const liftsVal = document.getElementById('liftsVal');
 const submitBtn = document.getElementById('submitBtn');
+const backBtn = document.getElementById('backBtn');
 const display = document.getElementById('display');
 const allLvl = document.getElementById('allLevelsDiv');
-
-let flVal;
-// let lftVal ;
+let flVal, lftVal = 2;
 let flArr = [];
-// let lftArr = [];
+let floorLevel;
+let lftArr = [];
 
 floorsVal.addEventListener('input',(e) => {
-  flVal = (e.target.value);
+  return flVal = (e.target.value);
 })
-
-// liftsVal.addEventListener('input',(e) => {
-//   lftVal = (e.target.value);
-// })
 
 submitBtn.addEventListener('click',() => {
   for (var i=1; i<= flVal ; i++){
     flArr.push(i);
   }
-  // for (var i=1; i<= lftVal ; i++){
-  //   lftArr.push(i);
-  // }
-  display.innerText = (flArr)
-  flArr.forEach((i)=>{
-    let item = document.createElement('div');
-    item.textContent = i;
-    item.setAttribute("id","levelDivMain")
-    allLvl.appendChild(item);
-  });
+  for (var i=1; i<= lftVal; i++){
+    lftArr.push(i);
+  }
+  createFloors(flArr);
+  createLifts(lftArr);
   flArr.splice(0, flVal);
-  console.log(flArr);
-  // lftArr.splice(0,lftVal);
+  submitBtn.style.display = 'none';
+  backBtn.style.display = 'block';
 })
+
+backBtn.addEventListener('click',()=>{
+  allLvl.replaceChildren();
+  backBtn.style.display = 'none';
+  submitBtn.style.display = 'block';
+  floorsVal.value = '';
+})
+
+function createFloors(arr){
+  arr.forEach((level)=>{
+    floorLevel = document.createElement('div');
+    floorLevel.setAttribute("id","levelDivMain");
+    allLvl.appendChild(floorLevel);
+    
+    let levelTitle = document.createElement('div');
+    levelTitle.setAttribute("id","levelTitle");
+    levelTitle.textContent = ("Floor "+ level);
+    floorLevel.appendChild(levelTitle);
+
+    let buttonBoard = document.createElement('div');
+    buttonBoard.setAttribute("id","buttonBoard");
+    floorLevel.appendChild(buttonBoard);
+
+    let buttonUp = document.createElement('button');
+    buttonUp.setAttribute("id","buttonUp");
+    buttonUp.textContent = "Up";
+    if(level < flVal){
+      buttonBoard.appendChild(buttonUp);
+    }
+
+    let buttonDown = document.createElement('button');
+    buttonDown.setAttribute("id","buttonDown");
+    buttonDown.textContent = "Down";
+    if(level > 1){
+      buttonBoard.appendChild(buttonDown);
+    }
+  })
+}
+
+function createLifts(liftArr){
+  liftArr.forEach((lift)=>{
+    let liftBox = document.createElement('div');
+    liftBox.setAttribute("id","liftBox");
+    liftBox.textContent = ("Lift "+ lift);
+    display.appendChild(liftBox);
+  })
+}
